e0471e38937e3220a72b8e41c8b82536901d4d58
Graham <graham.a.ross@gmail.com>
2021-02-17 10:07:36 -0500

    Added queue for json messages

--- code/Python/WebHost/server.py
+++ code/Python/WebHost/server.py
@@ -4,6 +4,7 @@ from flask import Flask, Response, request, url_for, render_template, jsonify
 from gevent.pywsgi import WSGIServer
 import json
 import sys
+import queue
 from settings import settings
 from mqtt import run, mqttPublish
 from logger import log
@@ -11,30 +12,34 @@ from logger import log
 # --------------------------------------------------------------------------- # 
 # Counters and status variables
 # --------------------------------------------------------------------------- # 
-theMessage                  = "{nothing}"
+theQueue                  = queue.Queue()
 cellCount                   = 0
+current_operation           = "Monitor"
 
 app = Flask(__name__)
 app.register_blueprint(settings, url_prefix="")
 
 @app.route("/")
 def render_index():
-   return render_template("index.html", cellCount = cellCount)
+   return render_template("index.html", operation = current_operation, cellCount = cellCount)
 
 @app.route('/operation', methods=['POST'])
 def operation():
-  operation = request.form.get("operation")
-  log.debug(operation)
-  mqttPublish(operation, "operation")
+  global operation
+  current_operation = request.form.get("operation")
+  log.debug(current_operation)
+  mqttPublish(current_operation, "operation")
   return jsonify(status="success")
 
 @app.route("/listen")
 def listen():
 
   def respond_to_client():
-    global theMessage
-    log.debug(theMessage)
-    yield f"id: 1\ndata: {theMessage}\nevent: online\n\n"
+    global theQueue
+    while  not theQueue.empty():
+      theMessage = theQueue.get()
+      log.debug(theMessage)
+      yield f"id: 1\ndata: {theMessage}\nevent: online\n\n"
   return Response(respond_to_client(), mimetype='text/event-stream')
   
 # --------------------------------------------------------------------------- # 
@@ -43,29 +48,31 @@ def listen():
 
 def on_stat(client, userdata, message):
 
-        global theMessage
+        global theQueue
         msg = message.payload.decode(encoding='UTF-8').upper()
         log.debug("Received STAT  {} : {}".format(message.topic, msg))
         message = json.loads(message.payload.decode(encoding='UTF-8'))
         theMessage = json.dumps(message)
+        theQueue.put(theMessage)
 
 def on_tele(client, userdata, message):
 
-        global theMessage, cellCount
+        global theQueue, cellCount
         msg = message.payload.decode(encoding='UTF-8').upper()
         log.debug("Received TELE  {} : {}".format(message.topic, msg))
         if "ping" in message.topic:
-          try:
             cellCount += 2
             log.debug("cellCount  {} ".format(cellCount))
-          except Exception as e: #LWT
-            log.debug("BatteryTester Online")
+
 
 def on_cmnd(client, userdata, message):
 
-        global theMessage
+        global theQueue
         msg = message.payload.decode(encoding='UTF-8').upper()
         log.debug("Received CMND  {} : {}".format(message.topic, msg))
+        if "operation" in message.topic:
+            log.debug("Todo Operation")
+
 
 if __name__ == "__main__":
   run(on_stat, on_tele, on_cmnd)

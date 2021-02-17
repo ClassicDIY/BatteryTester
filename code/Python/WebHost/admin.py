c23446e9291795b552a30a88285d17ab6d8983a9
Graham <graham.a.ross@gmail.com>
2021-02-17 07:36:53 -0500

    Revert "Slit python MQTT code, added grid"
    This reverts commit b9349b28e85a1ec7bce137e3c580c365011262d6.

--- code/Python/WebHost/admin.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, blueprints, render_template
-
-admin = Blueprint("admin", __name__, static_folder="static", template_folder="templates")
-
-@admin.route("/home")
-@admin.route("/")
-def home():
-    return render_template("home.html")

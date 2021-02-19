cf74a4a64e652b1f1d9d447fb15fbf0b85bde594
Graham <graham.a.ross@gmail.com>
2021-02-19 15:50:21 -0500

    Added PID to log

--- code/Python/WebApp/flask/app/logger.py
+++ code/Python/WebApp/flask/app/logger.py
@@ -9,7 +9,7 @@ import sys
 log = logging.getLogger('battery_tester')
 if not log.handlers:
     handler = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+    formatter = logging.Formatter('(%(process)d):%(name)s:%(message)s')
     handler.setFormatter(formatter)
     log.addHandler(handler) 
     log.setLevel(os.environ.get("LOGLEVEL", "DEBUG"))

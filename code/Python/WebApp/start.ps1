c43a33af9f7aa0d1c2a3fd65c0fcdabbecc71786
Graham <graham.a.ross@gmail.com>
2021-03-04 12:54:17 -0500

    added prune

--- code/Python/WebApp/start.ps1
+++ code/Python/WebApp/start.ps1
@@ -1,5 +1,6 @@
 $app = 'battery.tester'
-
+docker container stop 'BatteryTester'
+docker container prune -f
 docker build -f Dockerfile -t $app .
 $HostName = hostname.exe 
 docker run -p 80:80  -e HostName=$HostName --name 'BatteryTester' $app
